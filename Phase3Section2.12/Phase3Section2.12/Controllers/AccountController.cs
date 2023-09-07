4d46cb3c05bfacf0742268e06bd5031f46f0ee20
jeeban <jeebanjyoti193@gmail.com>
2023-09-07 09:27:04 +0530

    first commit

---
+++ Phase3Section2.12/Phase3Section2.12/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Phase3Section2._12.Models;
+using System.Security.Claims;
+
+
+namespace Phase3Section2._12.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
